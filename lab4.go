3af6faf2b56784732070967cbccb3f84e8a1c153
InnaPya <youwarkee006@gmail.com>
2017-01-23 11:28:11 +0700

    tggg

--- lab4.go
+++ lab4.go
@@ -4,17 +4,20 @@ type Token struct {
 	data string
 	recipient int
 }
-func grtn(i int, t Token, c chan string) {
-    	if (i != t.recipient) {
-		go grtn(i+1, t, c)
-	} else {
-		c<-strconv.Itoa(i) + " " + t.data
+func grtn(i int, n int, t Token, c chan string) {
+	fmt.Println("Thread", i, "is running")
+    	if (i == t.recipient) {
+		c<-"Thread " + strconv.Itoa(i) + " got " + t.data
+		go grtn(i+1, n, t, c)
+	} else if (i < n) {
+		go grtn(i+1, n, t, c)
 	}
 }
 func main() {
 	c := make(chan string)
 	N := 10
-	t := Token{"message", N}
-	go grtn(1, t, c)
+	dstn := 6
+	t := Token{"a message", dstn}
+	go grtn(1, N, t, c)
 	fmt.Println(<-c)
 }
